e39af13256ad373ac0c5760d04f76781f31f6dc3
Julian Stecklina <js@alien8.de>
2009-03-17 19:27:19 +0100

    nova-attach command for gdb

--- build/nova-gdb.py
+++ build/nova-gdb.py
@@ -1,20 +1,28 @@
 # -*- Mode: Python -*-
 import gdb
 
-print "Nova command set loading"
+class Attach_command(gdb.Command):
+    def __init__(self):
+        super(Attach_command, self).__init__("nova-attach", gdb.COMMAND_NONE)
+
+    def invoke(self, arg, from_tty):
+        gdb.execute("target remote localhost:1235")
 
 class Sc_printer:
     def __init__(self, val):
         self.val = val
 
     def to_string(self):
-        return 'prio %#2x (unfinished)'.format(self.val['prio'])
+        return 'prio %#2x (unfinished)' % self.val['prio']
 
 def make_nova_printer(val):
     if str (val.type()) == 'Sc':
         return Sc_printer(val)
     return None
 
-print "Done"
+Attach_command()
+gdb.pretty_printers.append(make_nova_printer)
+
+print "Type 'nova-attach' to attach to a running Nova kernel."
 
 # EOF
