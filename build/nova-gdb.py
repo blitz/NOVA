018484a8d6cb95176814baa3048293d1b9ecfca3
Julian Stecklina <js@alien8.de>
2009-04-11 16:48:44 +0200

    Benchmark stepi

--- build/nova-gdb.py
+++ build/nova-gdb.py
@@ -1,5 +1,6 @@
 # -*- Mode: Python -*-
 import gdb
+from time import time
 
 class Attach_command(gdb.Command):
     def __init__(self):
@@ -7,6 +8,18 @@ class Attach_command(gdb.Command):
 
     def invoke(self, arg, from_tty):
         gdb.execute("target remote localhost:1235")
+
+class Benchstepi_command(gdb.Command):
+    def __init__(self):
+        super(Benchstepi_command, self).__init__("nova-stepi", gdb.COMMAND_NONE)
+
+    def invoke(self, arg, from_tty):
+        repeat = 15
+        start = time()
+        for i in range(repeat):
+            gdb.execute("stepi")
+        end = time()
+        print("stepi took %f" % ((end - start) / repeat))
 
 class Sc_printer:
     def __init__(self, val):
@@ -21,6 +34,7 @@ def make_nova_printer(val):
     return None
 
 Attach_command()
+Benchstepi_command()
 gdb.pretty_printers.append(make_nova_printer)
 
 print "Type 'nova-attach' to attach to a running Nova kernel."
